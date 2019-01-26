145a37e8f8a9d9b8c22d9cf3e6b99a6cc5921301
Manbir Parmar <manbir.parmar27@gmail.com>
2019-01-25 17:53:09 -0800

    Finish chapter 10 exercises

--- chapter-10-exercises/exercise-1/exercise.R
+++ chapter-10-exercises/exercise-1/exercise.R
@@ -2,28 +2,28 @@
 
 # Create a vector of the number of points the Seahawks scored in the first 4 games
 # of the season (google "Seahawks" for the scores!)
-
+points <- c(27,38,23,21)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each of the first 4 games of the season
-
+opp_points <- c(24,31,26,7)
 
 # Combine your two vectors into a dataframe called `games`
-
+games <- data.frame(points, opp_points)
 
 # Create a new column "diff" that is the difference in points between the teams
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
-
+games$dif <- games$points - games$opp_points
 
 # Create a new column "won" which is TRUE if the Seahawks won the game
-
+games$won <- games$dif > 0
 
 # Create a vector of the opponent names corresponding to the games played
-
+opp_names <- c("Cardinals", "Chiefs", "49ers", "Vikings")
 
 # Assign your dataframe rownames of their opponents
-
+rownames(games) <-opp_names
 
 # View your data frame to see how it has changed!
-
+View(games)
--- chapter-10-exercises/exercise-5/exercise.R
+++ chapter-10-exercises/exercise-5/exercise.R
@@ -2,32 +2,38 @@
 
 # Read in the female baby names data file found in the `data` folder into a 
 # variable called `names`. Remember to NOT treat the strings as factors!
-
+baby_names <- read.csv("data/female_names.csv", stringsAsFactors = FALSE)
 
 # Create a data frame `names_2013` that contains only the rows for the year 2013
-
+baby_names_2013 <- baby_names$year == 2013
 
 # What was the most popular female name in 2013?
-
+most_popular_female_name_2013 <- baby_names_2013[baby_names_2013$prop == max(baby_names_2013$prop), "name"]
 
 # Write a function `most_popular_in_year` that takes in a year as a value and 
 # returns the most popular name in that year
-
+most_popular_in_year <- function(year) {
+  names_year <- baby_names[baby_names$year == year, ]
+  most_popular <- names_year[baby_names_year$prop == max(baby_names_year$prop), "name"]
+}
 
 # What was the most popular female name in 1994?
-
+most_popular_1994 <- most_popular_in_year(1994)
 
 # Write a function `number_in_million` that takes in a name and a year, and 
 # returns statistically how many babies out of 1 million born that year have 
 # that name. 
 # Hint: get the popularity percentage, and take that percentage out of 1 million.
-
+number_in_million <- function(name, year) {
+  name_popularity <- names[baby_names$year == year & baby_names$name == name, "prop"]
+  round(name_popularity * 1000000, 1)
+}
 
 # How many babies out of 1 million had the name 'Laura' in 1995?
-
+number_in_million("Laura", 1995)
 
 # How many babies out of 1 million had your name in the year you were born?
-
+number_in_million("Manbir", 1998) # I expect zero lol
 
 ## Consider: what does this tell you about how easy it is to identify you with 
 ## just your name and birth year?
